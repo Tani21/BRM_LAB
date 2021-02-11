092f741716e867ffaa1e31c83d16158be057400d
Tanishka Vaswani <tanishkavaswani1@gmail.com>
2021-02-11 18:23:43 +0530

    Second commit

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -4,6 +4,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Jenkins");
+		System.out.println("Second commit to check continuous integration");
+		
 	}
 
 }
